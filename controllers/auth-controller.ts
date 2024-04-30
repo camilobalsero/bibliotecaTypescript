c7b28fd2dd132820e01f2eda40a5a2ffc128d36c
Camilo Balsero <camilobalsero16@gmail.com>
2024-04-30 06:55:49 -0500

    se agrego el jwt en la autenticacion

--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -1,19 +1,23 @@
 const bcrypt = require("bcryptjs");
 const db = require('../config/config-db.js');
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import UserRepository from "../repositories/UserRepository";
 
 let auth = async (req: Request, res: Response) => {
     try {
         const {email, password} = req.body;
         const result : any= await UserRepository.login(email);
-        console.log(333, result);
-        console.log(password);
+
         if (result[0].length > 0){
             const isPasswordValid = await bcrypt.compare(password, result[0][0].password);
+
             if (isPasswordValid){
+                
+                const token = jwt.sign({ email: email }, "camilo", { expiresIn: "1h" });
                 return res.status(200).json({ 
-                    status: 'Successful authentication'
+                    status: 'Successful authentication',
+                    token: token 
                 });
             }
         }
